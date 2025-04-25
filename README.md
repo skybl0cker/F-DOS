74e553bbca40ab398fca074c8326d581a3420ad8
Cash Egley <skybl0ckerdev@gmail.com>
2025-04-25 15:26:05 -0400

    files

--- README.md
+++
@@ -1,2 +0,0 @@
-# F-DOS
-A mini dos-like operating system made by Cash, Luke, and Timothy for our Coding Practicum HS class.
---
+++ build/CMakeFiles/dos_emulator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dos_emulator.dir/src/main.cpp.o"
+  "CMakeFiles/dos_emulator.dir/src/main.cpp.o.d"
+  "CMakeFiles/dos_emulator.dir/src/pong.cpp.o"
+  "CMakeFiles/dos_emulator.dir/src/pong.cpp.o.d"
+  "CMakeFiles/dos_emulator.dir/src/shell.cpp.o"
+  "CMakeFiles/dos_emulator.dir/src/shell.cpp.o.d"
+  "CMakeFiles/dos_emulator.dir/src/snake.cpp.o"
+  "CMakeFiles/dos_emulator.dir/src/snake.cpp.o.d"
+  "dos_emulator"
+  "dos_emulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dos_emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
