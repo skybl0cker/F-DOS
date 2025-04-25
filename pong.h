ebb2d9ec6e03666ed7906986325dd7f35c0f6774
Cash Egley <142905945+skybl0cker@users.noreply.github.com>
2025-04-25 15:18:26 -0400

    add files

---
+++ pong.h
@@ -0,0 +1,8 @@
+#ifndef PONG_H
+#define PONG_H
+
+namespace pong {
+    void runPong();
+}
+
+#endif // PONG_H
---
+++ snake.h
@@ -0,0 +1,8 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+namespace snake {
+    void runSnake();
+}
+
+#endif 
