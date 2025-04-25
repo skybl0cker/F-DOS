29572cd9555181b19a62da5ccbae73711a8fdcde
Cash Egley <142905945+skybl0cker@users.noreply.github.com>
2025-04-25 15:21:23 -0400

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// main.cpp
+#include <iostream>
+#include "shell.h" // Include shell.h to make the Shell class visible
+
+int main() {
+    // Create the Shell object and start the shell
+    Shell shell;
+    shell.start();
+    return 0;
+}
